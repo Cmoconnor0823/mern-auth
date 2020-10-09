b40856e570a9427068626179ee6966ea43fdf5cc
Caitlin <47905793+Cmoconnor0823@users.noreply.github.com>
2020-10-09 16:44:40 -0700

    completed login validator

---
+++ validation/login.js
@@ -0,0 +1,25 @@
+// Simmilar to the register validator
+// This page is used for the log in
+
+module.exports = function validateLoginInput(data) {
+  let errors = {};
+
+  data.email = !isEmpty(data.email) ? data.email : "";
+  data.password = !isEmpty(data.password) ? data.password : "";
+
+  // Check for email
+  if (Validator.isEmpty(data.email)) {
+    errors.email = "Email field is required";
+  } else if (!Validator.isEmail(data.email)) {
+    errors.email = "Email is invalid";
+  }
+
+  if (Validator.isEmpty(data.password)) {
+    errors.password = "Please enter a password";
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors),
+  };
+};
