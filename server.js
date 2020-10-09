2f109a63a435060002488cd11ea93bef154a761a
Caitlin <47905793+Cmoconnor0823@users.noreply.github.com>
2020-10-08 23:34:12 -0700

    server is up and db is connected

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+require('dotenv').config();
+
 
 const app = express();
 
+// Bodyparser middleware //
+app.use(bodyParser.urlencoded({
+    extended: false
+})
+);
+app.use(bodyParser.json());
+
+// Connect to Database
+mongoose.connect(process.env.MONGO || URI,{ useNewUrlParser: true,  useUnifiedTopology: true })
+    .then(() => console.log("MongoDB successfully connected"))
+    .catch(err => console.log(err));
+
+const port = process.env.PORT;
+
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
