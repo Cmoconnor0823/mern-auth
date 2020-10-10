53d44bc9488508fb8f5fddd47a0bc01519e19b8c
Caitlin <47905793+Cmoconnor0823@users.noreply.github.com>
2020-10-09 22:31:59 -0700

    server up and working fixed error line 12 passport.js

--- config/keys.js
+++ config/keys.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const localmd = process.env.MONGO;
 const cloudmd = process.env.URI;
-console.log(localmd, "this should pring the local db connection");
+console.log(localmd, "this should bring the local db connection");
  
 module.exports = {
     mongoLocalURI: localmd,
