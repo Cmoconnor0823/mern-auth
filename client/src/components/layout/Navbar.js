b54aa815c13be64743b6552c3f56c31732e67799
Caitlin <47905793+Cmoconnor0823@users.noreply.github.com>
2020-11-01 10:58:01 -0700

    files added for landing and navbar has ben created

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class Navbar extends Component {
+    render() { 
+        return (
+            <div className="navbar-fixed">
+                <nav className="z-depth-0">
+                    <div className="nav-wrapper black">
+                        <Link 
+                        to="/"
+                        style={{
+                            fontFamily: "sans-serif"
+                        }}
+                        className="col s5 brand-logo center white-text">
+                            <i className="material-icons">Navbar test</i>
+                            MERN
+                        </Link>
+                    </div>
+                </nav>
+            </div>
+        );
+    }
+}
+
+export default Navbar;
