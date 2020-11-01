8625406e4155fdc5e20502128c1f944eb40f00ba
Caitlin <47905793+Cmoconnor0823@users.noreply.github.com>
2020-11-01 16:44:48 -0700

    completed auth reducers

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,3 +1,6 @@
+// Below we define the action types we need for our
+// redux store
+
 export const GET_ERRORS = "GET_ERRORS";
 export const   USER_LOADING = "USER_LOADING";
 export const  SET_CURRENT_USER ="SET_CURRENT_USER";
--- client/src/store.js
+++ client/src/store.js
@@ -1,10 +1,16 @@
+// Below we import everything needed for redux 
+
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 
+// Then we create our initial state and a variable
+// to use our middleware
 const initialState = {};
 
 const middleware = [thunk];
 
+//Next we create our store with optional parameters
+// to take advantage of redux dev tools
 const store = createStore (
     () => [],
     initialState,
@@ -14,4 +20,5 @@ const store = createStore (
     )
 );
 
+// finally we export the store so our app has access to it
 export default store;
