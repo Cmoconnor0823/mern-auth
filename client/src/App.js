c2b0b8163cab0baf191f65f1c925d6a7e508c710
Caitlin <47905793+Cmoconnor0823@users.noreply.github.com>
2020-11-01 11:29:31 -0700

    navbar and landing complete issue with react-dom and links

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
 import './App.css';
+import Nav from "./components/layout/Navbar"
+import Landing from "./components/layout/Landing";
 
 class App extends Component {
   render(){
   return (
     <div className="App">
+        <Nav />
+        <Landing />
       <h1>Hello</h1>
     </div>
   );
---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,58 @@
+import React,{ Component } from "react";
+import { Link } from "react-router-dom";
+
+class Landing extends Component {
+  render() {
+    return (
+      <div style={{ height: "70vh" }} className="container valign-wrapper">
+        <div className="row">
+          <div className="col s12 center-align">
+            <h3>
+              Template Mern application with authentication using passport and
+              JWT
+            </h3>
+            <p
+              style={{ fontFamily: "monospace" }}
+              className="flow-text white-text text-darken-1"
+            >
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris id
+              ultrices metus, eu fringilla enim. Cras ac risus vitae diam
+              ullamcorper sollicitudin. Integer viverra blandit libero vel
+              aliquam. Integer id nisi eu augue lobortis tristique eu in felis.{" "}
+            </p>
+            <br />
+            <div className="col s6">
+              <Link
+                to="/register"
+                style={{
+                  width: "20vw",
+                  borderRadius: "5px",
+                  letterSpacing: "2px",
+                }}
+                className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+              >
+                Register
+              </Link>
+            </div>
+            <div className="col s6">
+              <Link
+                to="/login"
+                style={{
+                  width: "20vw",
+                  borderRadius: "5px",
+                  letterSpacing: "2px",
+                }}
+                className="btn btn-large waves-effect waves-light hoverable black white-text accent-3"
+              >
+                Log In
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+export default Landing;
